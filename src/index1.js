0567653cbe7810c7b4852e185eb2da590437521b
zhoubing <254784109@qq.com>
2017-07-05 10:20:32 +0800

    提交一个版本

--- src/index1.js
+++ src/index1.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['hello', 'webpack'],'');
-   
+    element.classList.add('hello');
+
     return element;
 }
 
--- webpack.config.asset.js
+++ webpack.config.asset.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index1.js',
     output: {
         filename: 'bundle1.js',
         path: path.resolve(__dirname, 'dist')
